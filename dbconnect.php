f8a1d2c3b1887cc8ee7ac6cf89d0079dfb647197
Ayuka-Okada <a.snow.rsc2@gmail.com>
2020-06-30 12:37:22 +0900

    initial commit

---
+++ dbconnect.php
@@ -0,0 +1,38 @@
+<?php
+
+class DbManager
+{
+    public $dbh;
+
+    public function connect()
+    {
+        //DBに接続
+        $host = "localhost";
+        // データベース名を変えた場合はこちらを変更する（$dbname = "データベース名"）
+        $dbname = "todo_team";
+        $charset = "utf8mb4";
+        $user = 'root';
+        $password = '';
+
+        // $host = getenv('host'); //MySQLがインストールされてるコンピュータ
+        // $dbname = getenv('dbname'); //使用するDB
+        // $charset = "utf8"; //文字コード
+        // $user = getenv('username'); //MySQLにログインするユーザー名
+        // $password = getenv('password'); //ユーザーのパスワード
+
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        try {
+            $this->dbh = new PDO($dsn, $user, $password, $options);
+        } catch (\PDOException $e) {
+            var_dump($e->getMessage());
+            exit;
+        }
+
+    }
+}
